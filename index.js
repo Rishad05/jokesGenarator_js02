c987463fc4f8cb354eff109ecda15f75c95af60a
Rishad05 <iu.rishad5@gmail.com>
2021-12-14 15:42:41 +0600

    jokes genarator

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const jokeShow = document.querySelector("#jokeShow");
 const nextJoke = document.querySelector("#joke");
 
-const genarateJokes = () => {
-    const setHeader ={
-        headers:{
-            Accept: 'application/json'
-        }
+// const genarateJokes = () => {
+//     const setHeader ={
+//         headers:{
+//             Accept: 'application/json'
+//         }
+//     };
+//   fetch('https://icanhazdadjoke.com/', setHeader)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeShow.innerHTML = data.joke;
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
+
+const genarateJokes = async () => {
+  try {
+    const setHeader = {
+      headers: {
+        Accept: "application/json",
+      },
     };
-  fetch('https://icanhazdadjoke.com/', setHeader)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeShow.innerHTML = data.joke;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    const res = await fetch("https://icanhazdadjoke.com/", setHeader);
+    const data = await res.json();
+    jokeShow.innerHTML = data.joke;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 nextJoke.addEventListener("click", genarateJokes);
